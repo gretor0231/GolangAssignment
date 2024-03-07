a83cb6b414bff970b46a868bb7c92bffec39f738
gretor0231 <gretor0231@gmail.com>
2024-03-07 22:58:46 +0800

    added handlers.go.

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -12,3 +12,30 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
+
+func (app *Config) Add(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Add a person to the list",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) Remove(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Remove a person to the list",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) Query(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Query a person to the list",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -12,18 +12,25 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	// specify who is allowed to connect
-	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
+	mux.Use(
+		cors.Handler(
+			cors.Options{
+				AllowedOrigins:   []string{"https://*", "http://*"},
+				AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+				AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+				ExposedHeaders:   []string{"Link"},
+				AllowCredentials: true,
+				MaxAge:           300,
+			},
+		),
+	)
 
 	mux.Use(middleware.Heartbeat("/ping"))
 
 	mux.Post("/", app.Broker)
+	mux.Post("/Add", app.Add)
+	mux.Post("/Remove", app.Remove)
+	mux.Post("/Query", app.Query)
 
 	return mux
 }
