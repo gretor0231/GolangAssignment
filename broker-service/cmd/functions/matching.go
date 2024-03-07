1f56dc3f5f209419b4d8d775cdf629743233acaa
gretor0231 <gretor0231@gmail.com>
2024-03-07 15:46:13 +0800

    added two functions.

--- broker-service/cmd/functions/matching.go
+++ broker-service/cmd/functions/matching.go
@@ -1,11 +1,52 @@
 package functions
 
-func AddSinglePersonAndMatch() {
-
+type Person struct {
+	Name              string   `json:"name"`
+	Height            int      `json:"height"`
+	Gender            Gender   `json:"gender"`
+	WantedDatesNumber int      `json:"wantedDatesNumber"`
+	matcher           []string `json:"matcher"`
 }
 
-func RemoveSinglePerson() {
+type Gender struct {
+	IsMale bool `json:"isMale"`
+}
 
+func AddSinglePersonAndMatch(people []Person, person Person) []Person {
+	people = append(people, person)
+	for _, v := range people {
+		if v.Gender.IsMale == true && person.Gender.IsMale == false && v.Height > person.Height {
+			v.matcher = append(v.matcher, person.Name) // add person name to matcher list
+		}
+	}
+	return people
+}
+
+func RemoveSinglePerson(people []Person, name string) []Person {
+
+	//corner case
+	if len(people) <= 0 {
+		return people
+	}
+	//base case
+	if len(people) == 1 && people[0].Name == name {
+		people = []Person{}
+		return people
+	}
+	//normal case
+	for i, v := range people {
+		if v.Name == name {
+			people = append(people[:i], people[i+1:]...)
+		}
+	}
+	for _, v := range people {
+		for i, matchName := range v.matcher {
+			if matchName == name {
+				v.matcher = append(v.matcher[:i], v.matcher[i+1:]...)
+			}
+		}
+	}
+	return people
 }
 
 func QuerySinglePeople() {
--- broker-service/cmd/model/person.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type person struct {
-	Name              string `json:"name"`
-	Height            int    `json:"height"`
-	Gender            string `json:"gender"`
-	WantedDatesNumber int    `json:"wantedDatesNumber"`
-}
